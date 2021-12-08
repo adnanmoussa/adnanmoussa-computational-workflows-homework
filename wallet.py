8506af63498522084464ad1e210e1372c401400e
adnanmoussa <51108326+adnanmoussa@users.noreply.github.com>
2021-12-08 21:49:44 +0100

    updating wallet.py

--- wallet.py
+++ wallet.py
@@ -6,10 +6,12 @@ class InsufficientAmount(Exception):
 
 class Wallet(object):
     def __init__(self, initial_amount=0):
-        raise NotImplementedError
+        raise self.balance = initial_amount
 
     def spend_cash(self, amount):
-        raise NotImplementedError
+        if (self.balance < amount):
+            raise InsufficientAmount
+        self.balance -= amount
 
     def add_cash(self, amount):
-        raise NotImplementedError
+        self.balance += amount
